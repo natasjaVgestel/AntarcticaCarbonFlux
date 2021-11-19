e6f11488b1eac2ad00cd5b8fa10a2f010c0d4cc7
[rmcnellis] <[risa.mcnellis@gmail.com]>
2021-11-19 11:00:13 -0500

    create treatment ID and clean up remaining redos

--- clean_carbonflux.R
+++ clean_carbonflux.R
@@ -1,9 +1,11 @@
 # script to clean Antarctica Carbon Flux data
 
+# load packages
 library(dplyr)
 library(tidyr)
 library(purrr)
 
+# read in data by site
 site1 <- list.files(path = "./site 1/",
              pattern = "*.csv", 
              full.names = T) %>% 
@@ -64,6 +66,7 @@ site4 <- list.files(path = "./site 4/",
                                                                 rep("character", 5),
                                                                 rep("numeric", 21))))
 
+# create list of column names
 col_names <- c("obs", "sys_time", "elapsed", "date", "sys_hhmmss", "vtot", "rho", "Flux", "virga", 
            "vchamber", "area", "R", "collar_offset", "plot", "measurement", "notes", 
            "gasex_time", "E", "A", "RHcham", "VPcham", "SVPcham", "Leak", "LeakPct", "CorrFact", 
@@ -79,21 +82,33 @@ col_names <- c("obs", "sys_time", "elapsed", "date", "sys_hhmmss", "vtot", "rho"
            "Tirga", "Tchopper", "Ts", "Tr", "CO2_%", "Desiccant_%", "Humidifier_%", 
            "Txchg_sp", "CO2_r_sp", "H2O_r_sp", "SS_s", "SS_r")
 
+# add column names
 names(site1) <- col_names
 names(site2) <- col_names
 names(site3) <- col_names
 names(site4) <- col_names
 
+# add site IDs
 site1$site <- rep(1)
 site2$site <- rep(2)
 site3$site <- rep(3)
 site4$site <- rep(4)
 
+# combine all sites into one dataset
 data_all <- bind_rows(site1, site2, site3, site4)
 
-data_all <- select(data_all, -obs)
-data_all <- select(data_all, date, site, plot, measurement, notes, everything())
+# create warming treatment column
+data_all <- separate(data = data_all, col = plot, into = c("plot_id", "treatment"), sep = "(?<=[0-9])(?=[A-Za-z])", remove = FALSE)
+data_all$treatment[data_all$treatment == "c"] <- "control"
+data_all$treatment[data_all$treatment == "w"] <- "warming"
 
+data_all <- select(data_all, -plot_id)
+
+# remove obs column and reorder remaining columns
+data_all <- select(data_all, -obs)
+data_all <- select(data_all, date, site, plot, treatment, measurement, notes, everything())
+
+# consolidate measurement naming
 data_all$measurement[data_all$measurement == "nee" |
                        data_all$measurement == "ne" |
                        data_all$measurement == "NEE " |
@@ -104,6 +119,7 @@ data_all$measurement[data_all$measurement == "er" |
 data_all$measurement[data_all$measurement == "NEE OR ER" |
                        data_all$measurement == "NEE or ER"] <- ""
 
+# consolidate notes
 data_all$notes[data_all$notes == "d" |
                            data_all$notes == "dr" |
                            data_all$notes == "black" |
@@ -126,6 +142,7 @@ data_all$notes[data_all$notes == "shade 1" |
                  data_all$notes == "sheer" |
                  data_all$notes == "sher" |
                  data_all$notes == "sheer1" |
+                 data_all$notes == "sheer cloth" |
                  data_all$notes == "sheer doubled" |
                  data_all$notes == "kelly shadow" |
                  data_all$notes == "shadow" |
@@ -134,8 +151,10 @@ data_all$notes[data_all$notes == "shade 1" |
                            data_all$notes == "dark2" |
                            data_all$notes == "darklight"] <- "shade"
 
+# remove shade measurements
 data_all <- subset(data_all, notes != "shade")
 
+# set missing measurement type for light and dark measurements and remove from notes
 data_all$measurement[data_all$notes == "light"] <- "NEE"
 data_all$measurement[data_all$notes == "dark"] <- "ER"
 
